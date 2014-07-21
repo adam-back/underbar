481e04ea3c5f1da9755a42c934d85959b8eb72a8
Adam Back <adb33@email.arizona.edu>
2014-07-20 17:50:40 -0700

    Some works except when passing the string 'yes' as a truth value.

--- src/underbar.js
+++ src/underbar.js
@@ -249,6 +249,8 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    var trues = _.filter(collection, iterator).length;
+    return trues > 0;
   };
 
 
